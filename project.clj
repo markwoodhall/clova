4c37f655adf64260c68d98b612bfbbc0feb6db9f
Mark Woodhall <mark.woodhall@gmail.com>
2015-11-14 22:49:26 +0000

    Increment version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clova "0.1.0"
+(defproject clova "0.2.0"
   :description "A simple validation library for Clojure and ClojureScript."
   :url "http://github.com/markwoodhall/clova"
   :license {:name "The MIT License"
