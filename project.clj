98fa36a8fd25ba506f53e4ce5b20cff68f1e887d
Mark Woodhall <mark.woodhall@gmail.com>
2015-11-15 21:33:18 +0000

    Fix codox source-uri, not sure why manually adding the x for cljx is required.

--- project.clj
+++ project.clj
@@ -8,7 +8,8 @@
   :deploy-repositories [
     ["clojars" {:sign-releases false}]
   ]
-  :codox {:namespaces [clova.core] :source-uri "https://github.com/markwoodhall/clova/blob/master/{filepath}#L{line}"}
+  :codox {:namespaces [clova.core]
+          :source-uri "https://github.com/markwoodhall/clova/blob/master/src/{classpath}x#L{line}"}
   :prep-tasks [["cljx-once"]]
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :source-paths ["src" "target/classes"]
