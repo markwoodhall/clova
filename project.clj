17e0e306b20b45e43ff959d53bcb8d96a782443e
Mark Woodhall <mark.woodhall@gmail.com>
2016-01-12 19:57:06 +0000

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clova "0.22.0"
+(defproject clova "0.23.0"
   :description "A simple validation library for Clojure and ClojureScript."
   :url "http://github.com/markwoodhall/clova"
   :license {:name "The MIT License"
